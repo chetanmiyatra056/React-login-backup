b758f9c2a44d578684616bb35bfd619ed64e6980
Chetan <chetanmiyatra056@gmail.com>
2024-08-02 09:48:11 +0530

    date update success api

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,22 +5,24 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-
     function register(Request $request)
     {
 
         $validator = Validator::make($request->all(), [
             'name' => 'required|unique:users',
             'email' => 'required|unique:users|regex:/(.+)@(.+)\.(.+)/i',
-            'options' => 'required',
             'countriesid' => 'required',
             'statesid' => 'required',
             'citiesid' => 'required',
+            'hobbies' => 'required|array',
+            'gender' => 'required',
+            'selectDate' => 'required',
             'password' => 'required|min:4|max:8',
             'confirm_password' => 'required|same:password|min:4|max:8',
         ]);
@@ -38,10 +40,19 @@ class RegisterController extends Controller
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->password = Hash::make($request->input('password'));
-        $user->hobbies = implode(',', $request->input("options"));
         $user->countries = $request->input('countriesid');
         $user->states = $request->input('statesid');
         $user->cities = $request->input('citiesid');
+        // $user->hobbies = implode(',', $request->input("hobbies"));
+        $user->hobbies = $request->input("hobbies");
+        $user->gender = $request->input('gender');
+
+        $_stockupdate = Carbon::parse($request["selectDate"])->format('Y-m-d');
+
+        $user->date = $_stockupdate;
+
+        // $user->date = $request->input('selectDate');
+        // $user->hobbies =  $request->input("hobbies");
         $user->save();
 
         // return $request->input();
@@ -49,6 +60,7 @@ class RegisterController extends Controller
         return response()->json([
             'message' => 'User registered successfully.',
             'status' => true,
+            'type' => 'success',
             'data' => $user,
         ], 201);
     }
--- app/Http/Controllers/Api/UpdateController.php
+++ app/Http/Controllers/Api/UpdateController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -20,14 +21,60 @@ class UpdateController extends Controller
         }
     }
 
+    // public function update(Request $request, $id)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         // 'name' => 'required|unique:users,name,' . $id,
+    //         // 'email' => 'required|unique:users,email,' . $id . '|regex:/(.+)@(.+)\.(.+)/i',
+    //         // 'countriesid' => 'required',
+    //         // 'statesid' => 'required',
+    //         // 'citiesid' => 'required',
+    //         'hobbies' => 'required',
+    //         'gender' => 'required',
+    //         'selectDate' => 'required',
+    //     ]);
+
+    //     if ($validator->fails()) {
+    //         return response()->json([
+    //             'message' => false,
+    //             'error' => $validator->errors(),
+    //             'status' => false,
+    //         ], 200);
+    //     }
+
+    //     $user = User::find($id);
+    //     if (!$user) {
+    //         return response()->json([
+    //             'message' => 'User not found',
+    //             'status' => false,
+    //         ], 404);
+    //     }
+
+    //     // $user->name = $request->input('name');
+    //     // $user->email = $request->input('email');
+    //     // $user->countries = $request->input('countriesid');
+    //     // $user->states = $request->input('statesid');
+    //     // $user->cities = $request->input('citiesid');
+    //     $user->hobbies = implode(',', $request->input("hobbies"));
+    //     $user->gender = $request->input('gender');
+
+    //     $_stockupdate = Carbon::parse($request["selectDate"])->format('Y-m-d');
+
+    //     $user->date = $_stockupdate;
+    //     $user->update();
+
+    //     return response()->json([
+    //         'message' => 'User updated successfully.',
+    //         'status' => true,
+    //         'data' => $user,
+    //     ], 201);
+    // }
     public function update(Request $request, $id)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|unique:users,name,' . $id,
-            'email' => 'required|unique:users,email,' . $id . '|regex:/(.+)@(.+)\.(.+)/i',
-            'countriesid' => 'required',
-            'statesid' => 'required',
-            'citiesid' => 'required',
+            'hobbies' => 'required',
+            'gender' => 'required',
+            'selectDate' => 'required|date_format:Y-m-d',
         ]);
 
         if ($validator->fails()) {
@@ -46,11 +93,9 @@ class UpdateController extends Controller
             ], 404);
         }
 
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->countries = $request->input('countriesid');
-        $user->states = $request->input('statesid');
-        $user->cities = $request->input('citiesid');
+        $user->hobbies = implode(',', $request->input("hobbies"));
+        $user->gender = $request->input('gender');
+        $user->date = $request->input('selectDate');
         $user->update();
 
         return response()->json([
@@ -59,4 +104,5 @@ class UpdateController extends Controller
             'data' => $user,
         ], 201);
     }
+
 }
