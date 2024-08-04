7b071bf123af1eb3680f2983b6a0cc816e618e52
Chetan <chetanmiyatra056@gmail.com>
2024-08-04 22:07:39 +0530

    image display completed api

--- app/Http/Controllers/Api/UpdateController.php
+++ app/Http/Controllers/Api/UpdateController.php
@@ -34,6 +34,7 @@ class UpdateController extends Controller
             'gender' => 'required',
             'selectDate' => 'required|date_format:Y-m-d',
             'userType' => 'required',
+            'file' => 'required',
         ]);
 
         if ($validator->fails()) {
@@ -52,7 +53,7 @@ class UpdateController extends Controller
             ], 404);
         }
 
-        $user->name =  $request->input('name');
+        $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->countries = $request->input('countriesid');
         $user->states = $request->input('statesid');
@@ -62,6 +63,14 @@ class UpdateController extends Controller
         $user->gender = $request->input('gender');
         $user->date = $request->input('selectDate');
         $user->type = $request->input('userType');
+
+        // Decode and save the file
+        $fileData = $request->input('file');
+        $fileName = time() . '.png';  // You can dynamically determine the file extension if necessary
+        $filePath = public_path('uploads') . '/' . $fileName;
+        file_put_contents($filePath, base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $fileData)));
+
+        $user->profile = $fileName;
 
         if ($user->update()) {
             return response()->json([
@@ -79,40 +88,4 @@ class UpdateController extends Controller
             ], 201);
         }
     }
-
-    // public function update(Request $request, $id)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'hobbies' => 'required',
-    //         'gender' => 'required',
-    //         'selectDate' => 'required|date_format:Y-m-d',
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response()->json([
-    //             'message' => false,
-    //             'error' => $validator->errors(),
-    //             'status' => false,
-    //         ], 200);
-    //     }
-
-    //     $user = User::find($id);
-    //     if (!$user) {
-    //         return response()->json([
-    //             'message' => 'User not found',
-    //             'status' => false,
-    //         ], 404);
-    //     }
-
-    //     $user->hobbies = implode(',', $request->input("hobbies"));
-    //     $user->gender = $request->input('gender');
-    //     $user->date = $request->input('selectDate');
-    //     $user->update();
-
-    //     return response()->json([
-    //         'message' => 'User updated successfully.',
-    //         'status' => true,
-    //         'data' => $user,
-    //     ], 201);
-    // }
 }
